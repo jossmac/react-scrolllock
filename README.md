b94ee84c6bc417469edcf67eef0dbd964136980e
Joss Mackison <joss@thinkmill.com.au>
2016-11-04 13:12:44 +1100

    prepare and document component

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# scrolllock
-Lock body scroll when mounted
+# React Scroll Lock
+
+Prevent scroll on the `<body />` when component is mounted.
+
+### Usage
+
+```bash
+npm install --save react-scrolllock
+```
+
+```js
+import ScrollLock from 'react-scrolllock';
+
+class Modal extends Component {
+  render () {
+    return (
+      <div>
+        ...
+        <ScrollLock />
+      </div>
+    )
+  }
+}
+```
---
+++ src/ScrollLock.js
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+
+/*
+	NOTES
+
+	1. Stop content jumping around when overflow is hidden on the body.
+	2. Mobile Safari ignores { overflow: hidden } declaration on the body.
+*/
+
+export default class ScrollLock extends Component {
+	constructor () {
+		super();
+		this.locked = false;
+	}
+
+	componentWillMount () {
+		if (!canUseDom) return;
+
+		const scrollbarWidth = window.innerWidth - document.body.clientWidth; // 1.
+		const target = document.body;
+
+		target.addEventListener('touchmove', preventTouchMove, false); // 2.
+
+		target.style.paddingRight = scrollbarWidth + 'px';
+		target.style.overflowY = 'hidden';
+
+		this.locked = true;
+	}
+
+	componentWillUnmount () {
+		if (!canUseDom) return;
+
+		const target = document.body;
+
+		target.removeEventListener('touchmove', preventTouchMove, false);
+
+		target.style.paddingRight = '';
+		target.style.overflowY = '';
+
+		this.locked = false;
+	}
+
+	render () {
+		return null;
+	}
+}
+
+function preventTouchMove (e) {
+	e.preventDefault();
+};
+
+function canUseDom () {
+	return !!(
+		typeof window !== 'undefined'
+		&& window.document
+		&& window.document.createElement
+	);
+};
