236a0c19564ef8169b60e40b1a8e1d8676377cff
Joss Mackison <joss@thinkmill.com.au>
2016-11-23 14:38:38 +1100

    allow scroll on provided target

--- src/ScrollLock.js
+++ src/ScrollLock.js
@@ -5,29 +5,45 @@ var React = require('react');
 
 	1. Stop content jumping around when overflow is hidden on the body.
 	2. Mobile Safari ignores { overflow: hidden } declaration on the body.
+	3. Allow scroll on provided target.
 */
 
 var ScrollLock = React.createClass({
-	componentWillMount: function () {
+	propTypes: {
+		scrollTarget: React.PropTypes.object,
+	},
+	componentDidMount: function () {
 		if (!canUseDom) return;
 
-		const scrollbarWidth = window.innerWidth - document.body.clientWidth; // 1.
-		const target = document.body;
-
-		target.addEventListener('touchmove', preventTouchMove, false); // 2.
+		var scrollTarget = this.props.scrollTarget;
+		var scrollbarWidth = window.innerWidth - document.body.clientWidth; // 1.
+		var target = document.body;
 
 		target.style.paddingRight = scrollbarWidth + 'px';
 		target.style.overflowY = 'hidden';
+
+		target.addEventListener('touchmove', preventTouchMove, false); // 2.
+
+		if (scrollTarget) {
+			scrollTarget.addEventListener('touchstart', preventInertiaScroll, false); // 3.
+			scrollTarget.addEventListener('touchmove', allowTouchMove, false); // 3.
+		}
 	},
 	componentWillUnmount: function () {
 		if (!canUseDom) return;
 
-		const target = document.body;
-
-		target.removeEventListener('touchmove', preventTouchMove, false);
+		var scrollTarget = this.props.scrollTarget;
+		var target = document.body;
 
 		target.style.paddingRight = '';
 		target.style.overflowY = '';
+
+		target.removeEventListener('touchmove', preventTouchMove, false);
+
+		if (scrollTarget) {
+			scrollTarget.addEventListener('touchstart', preventInertiaScroll, false);
+			scrollTarget.addEventListener('touchmove', allowTouchMove, false);
+		}
 	},
 	render: function () {
 		return null;
@@ -37,6 +53,22 @@ var ScrollLock = React.createClass({
 function preventTouchMove (e) {
 	e.preventDefault();
 };
+
+function allowTouchMove (e) {
+	e.stopPropagation();
+};
+
+function preventInertiaScroll () {
+	var top = this.scrollTop;
+	var totalScroll = this.scrollHeight;
+	var currentScroll = top + this.offsetHeight;
+
+	if (top === 0) {
+		this.scrollTop = 1;
+	} else if (currentScroll === totalScroll) {
+		this.scrollTop = top - 1;
+	}
+}
 
 function canUseDom () {
 	return !!(
