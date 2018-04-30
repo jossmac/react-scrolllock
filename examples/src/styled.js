0decb00bbbecd7e6852bee292ba658da84095aa5
Joss Mackison <joss.mackison@gmail.com>
2018-04-30 11:57:21 +1000

    demo: add momentum scroll to touch area

--- examples/src/styled.js
+++ examples/src/styled.js
@@ -81,6 +81,7 @@ export const TouchScrollArea = ({ height, innerRef, ...props }) => (
       overflowY: 'auto',
       paddingLeft: gutter,
       paddingRight: gutter,
+      '-webkit-overflow-scrolling': 'touch',
 
       '@media (max-width: 420px)': {
         display: 'block',
